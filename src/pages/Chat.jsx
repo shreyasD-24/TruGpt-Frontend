f71de61d579d61c0cd8665db699f2451c69df975
Shreyas D <shreyas24d@gmail.com>
2024-05-01 00:04:33 +0530

    styled clear chat feauture for small screens

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -46,58 +46,67 @@ const Chat = () => {
   }, []);
 
   return (
-    <div className="flex my-10">
-      <div className="xs:w-[25%] max-xs:hidden h-[60vh] sticky top-32 items-center flex flex-col bg-slate-800 ml-10 rounded-2xl">
-        <div className="inline-flex items-center justify-center w-14 h-14 overflow-hidden bg-[rgb(255,255,255,1)] rounded-full mt-8">
-          <span className="text-2xl font-bold text-black">GPT</span>
-        </div>
-        <div className="text-sm font-semibold text-white my-2 text-center">
-          You are talking to a ChatBot !
-        </div>
-        <div className="text-sm font-semibold text-white my-2 text-center">
-          Avoid providing personal information in prompts.
-        </div>
-        <button
-          type="button"
-          onClick={clearConversation}
-          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br mt-auto font-medium rounded-full text-sm px-5 py-2.5 text-center my-6"
-        >
-          Clear Conversation
-        </button>
-      </div>
-      <div className="xs:mx-12 mx-4 w-full">
-        <div className="text-5xl text-bold font-[roboto] text-center mb-2 italic">
-          Gemini Pro AI
-        </div>
-        <div className="xs:w-[80%] w-full overflow-x-none overflow-y-auto mx-auto rounded-xl">
-          {chats.map((chat, index) => {
-            return (
-              <ChatItem
-                role={chat.role}
-                content={chat.parts[0].text}
-                key={index}
-              ></ChatItem>
-            );
-          })}
-        </div>
-        <div className="xs:w-[80%] w-full mx-auto z-10 mt-3 sticky bottom-10">
-          <textarea
-            rows="1"
-            className="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Ask any question..."
-            ref={query}
-          ></textarea>
+    <>
+      <button
+        type="button"
+        onClick={clearConversation}
+        className="text-white xs:hidden mt-2 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br text-md rounded-md flex ml-auto mr-8 text-sm px-1 py-1 text-center"
+      >
+        Clear Chat
+      </button>
+      <div className="flex my-10">
+        <div className="xs:w-[25%] max-xs:hidden h-[60vh] sticky top-32 items-center flex flex-col bg-slate-800 ml-10 rounded-2xl">
+          <div className="inline-flex items-center justify-center w-14 h-14 overflow-hidden bg-[rgb(255,255,255,1)] rounded-full mt-8">
+            <span className="text-2xl font-bold text-black">GPT</span>
+          </div>
+          <div className="text-sm font-semibold text-white my-2 text-center">
+            You are talking to a ChatBot !
+          </div>
+          <div className="text-sm font-semibold text-white my-2 text-center">
+            Avoid providing personal information in prompts.
+          </div>
           <button
             type="button"
-            onClick={askQuery}
-            className="focus:outline-none my-2 z-20 absolute right-[2px] bottom-[0.15rem] text-white bg-green-700 hover:bg-green-800 focus:ring-2 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            onClick={clearConversation}
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br mt-auto font-medium rounded-full text-sm px-5 py-2.5 text-center my-6"
           >
-            <IoSend />
+            Clear Conversation
           </button>
         </div>
-        <div className="xs:w-[80%] w-full h-[2.9rem] mx-auto sticky bottom-0 bg-[#05101c]"></div>
+        <div className="xs:mx-12 mx-4 w-full">
+          <div className="text-5xl text-bold font-[roboto] text-center mb-2 italic">
+            Gemini Pro AI
+          </div>
+          <div className="xs:w-[80%] w-full overflow-x-none overflow-y-auto mx-auto rounded-xl">
+            {chats.map((chat, index) => {
+              return (
+                <ChatItem
+                  role={chat.role}
+                  content={chat.parts[0].text}
+                  key={index}
+                ></ChatItem>
+              );
+            })}
+          </div>
+          <div className="xs:w-[80%] w-full mx-auto z-10 mt-3 sticky bottom-10">
+            <textarea
+              rows="1"
+              className="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Ask any question..."
+              ref={query}
+            ></textarea>
+            <button
+              type="button"
+              onClick={askQuery}
+              className="focus:outline-none my-2 z-20 absolute right-[2px] bottom-[0.15rem] text-white bg-green-700 hover:bg-green-800 focus:ring-2 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+              <IoSend />
+            </button>
+          </div>
+          <div className="xs:w-[80%] w-full h-[2.9rem] mx-auto sticky bottom-0 bg-[#05101c]"></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
